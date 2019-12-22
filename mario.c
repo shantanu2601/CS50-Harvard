b73d7ef178856acc34e801217ad693f211949bc1
shantanu2601 <59122752+shantanu2601@users.noreply.github.com>
2019-12-22 07:53:08 +0530

    Add files via upload

--- mario.c
+++ mario.c
@@ -5,21 +5,21 @@ int main(void)
 {
     for(;;)
     {
-    printf("Height: ");
-    int ht = get_int("");
-    printf("\n");
-    if(ht>=1 && ht<=8)
-    {
-        int i,j,k;
-        for(i=1;i<=ht;i++)
+        printf("Height: ");
+        int ht = get_int("");
+        printf("\n");
+        if(ht>=1 && ht<=8)
         {
-            for(k=i;k<ht;k++)
-                printf(" ");
-        
-            for(j=1;j<=i;j++)
+            int i,j,k,h;
+            for(i=1;i<=ht;i++)
             {
+                for(k=i;k<ht;k++)
+                printf(" ");   
+            for(j=1;j<=i;j++)
                 printf("#");
-            }
+            printf("  ");
+            for(h=1;h<=i;h++)
+                printf("#");
             printf("\n");
         }
         break;
